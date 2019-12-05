0d4d4ddfa3867b9a87be19bb9ec3b8ba9e7d462a
long <nguy4227@gmail.com>
2019-12-05 13:53:05 -0600

    mobile size phone photo resize

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default class Home extends React.Component {
         this.setState({width:'100%'})
       }
       else {
-        this.setState({width:'200%'})
+        this.setState({width:'150%'})
       };
   }
 
